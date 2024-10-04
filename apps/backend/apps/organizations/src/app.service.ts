dda25dcbeb37657b78c4ea40059fca154b7fa16a
Rodney Gagnon <rodneygagnon@mac.com>
2024-10-04 14:05:44 -0700

    fix broken husky

--- apps/backend/apps/organizations/src/app.service.ts
+++ apps/backend/apps/organizations/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getGreeting(): string {
-    return 'Welcome to the Organization App Service!';
+    return 'Welcome to the Organization App Service!!!';
   }
 }
